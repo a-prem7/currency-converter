c027c2d51c435a631c201e5dda77111f95487ac5
amal-prem <aldamal.prem@gmail.com>
2023-04-16 18:03:00 -0400

    declared variables

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const dropList = document.querySelectorAll("form select"),
+fromCurrency = document.querySelector(".from select"),
+toCurrency = document.querySelector(".to select"),
+getButton = document.querySelector("form button");
